3b37aad5eb534e755e88a3626d78b787c5578277
Tom <tomwilliamfranklin@gmail.com>
2019-12-22 15:09:15 +0000

    Finished mobile styling for iphone SE <

--- src/components/header.js
+++ src/components/header.js
@@ -5,9 +5,9 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header class="header">
     <div class="header-inner">
-    <a><span class="navigation-button">About Me</span></a>
-    <a><span class="navigation-button">Portfolio</span></a>
-    <a><span class="navigation-button">Blog</span></a>
+    <a><span id="" class="navigation-button">About Me</span></a>
+    <a><span id="" class="navigation-button">Portfolio</span></a>
+    <a><span id="" class="navigation-button">Blog</span></a>
     </div>
   </header>
 )
